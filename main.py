a12e903e0b2547526fddbc47126c7b6a47246459
coding938 <52285815+coding938@users.noreply.github.com>
2022-06-08 01:24:00 +0000

    up

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("hello")
+# import the package
+from pytube import YouTube
+
+url = 'https://www.youtube.com/watch?v=XNA3CqHdejk&list=RDMM2VXUE54TQ1I&index=7&ab_channel=HiMusicHiFlow'
+
+my_video = YouTube(url)
+
+print("*********************Video Title************************")
+# get Video Title
+print(my_video.title)
+
+print("********************Tumbnail Image***********************")
+# get Thumbnail Image
+print(my_video.thumbnail_url)
+
+print("********************Download video*************************")
+# get all the stream resolution for the
+for stream in my_video.streams:
+    print(stream)
+
+# set stream resolution
+my_video = my_video.streams.get_highest_resolution()
+
+# or
+#my_video = my_video.streams.first()
+
+# Download video
+my_video.download()
